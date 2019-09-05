6865a1afb4109b3cbcba96a17854cf9e3ceaae60
Dani <dani.pous@gmail.com>
2019-09-05 14:57:13 +0200

    Adding nixpkg for installing rdkafka 1.1.0

---
+++ nixpkgs/rdkafka.nix
@@ -0,0 +1,31 @@
+#{ stdenv, fetchFromGitHub, zlib, perl, pkgconfig, python, openssl }:
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  pname = "rdkafka";
+  version = "1.1.0";
+
+  src = fetchFromGitHub {
+    owner = "edenhill";
+    repo = "librdkafka";
+    rev = "v${version}";
+    sha256 = "0j1q99hq9k8v3w2yb5m8xqp1gx47yrajglmcf9mjbczsmm25driq";
+  };
+
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ zlib perl python openssl ];
+
+  NIX_CFLAGS_COMPILE = "-Wno-error=strict-overflow";
+
+  postPatch = ''
+    patchShebangs .
+  '';
+
+  meta = with stdenv.lib; {
+    description = "librdkafka - Apache Kafka C/C++ client library";
+    homepage = https://github.com/edenhill/librdkafka;
+    license = licenses.bsd2;
+    platforms = platforms.linux ++ platforms.darwin;
+    maintainers = with maintainers; [ boothead ];
+  };
+}
